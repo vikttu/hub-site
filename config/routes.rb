e44ca9e08dcc334cb14176746cac4664bfed4e3b
Viktor Nespolo <nespolopeixoto@outlook.com>
2022-05-19 20:24:31 -0300

    Create my root route and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "welcome#index"
+
+  resources :welcome, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
